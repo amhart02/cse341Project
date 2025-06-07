aa06b31c87283a24094b967ac5d83e8c68ab5c26
amhart02 <har20010@byui.edu>
2025-06-06 22:58:35 -0600

    Added the routes for the get, post, put, and delete for the user database

--- controllers/user.js
+++ controllers/user.js
@@ -1,1 +1,5 @@
-// to implement later
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+const isValidObject = (id) => ObjectId.isValid(id) && String(new ObjectId(id)) === id;
+
